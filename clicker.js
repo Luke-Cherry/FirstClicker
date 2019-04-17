0c1966893d2a0a7e0c4dbf732d0e37db3ff8ff2a
MrVisneski <38617758+MrVisneski@users.noreply.github.com>
2019-04-17 23:53:23 +0100

    Changed ducks and cleaned up code

--- clicker.js
+++ clicker.js
@@ -11,10 +11,19 @@ var defaultValues = {
     cost_increase_cost: 100,
     cost_increase_num: 1,
     cash: 0,
+    lifetimeCash: 0,
     eggcollectorNum: 0,
     eggcollectorCost: 10,
     click_increment: 1,
     click_increment_cost: 1
+};
+
+//Creates a NodeList with DOM of all elements on website that has a ID tag.
+var element = {};
+for (var el of document.querySelectorAll("*").values()){
+  if (!!(el.getAttribute("id"))){
+    element[el.id] = el;
+  }
 }
 
 var gameData = {
@@ -37,8 +46,9 @@ var gameData = {
     cost_increase_cost: 100,
     cost_increase_num: 1,
 
-//Current total cash
+//Current total cash + lifetimeCash
     cash: 0,
+    lifetimeCash: 0,
 
 //Shows no. of eggCollectors + the cost of each eggCollector (increases with each purchase).
     eggcollectorNum: 0,
@@ -46,9 +56,9 @@ var gameData = {
 
     click_increment: 1,
     click_increment_cost: 1
-}
+};
 
-var duckImages = ["pics\\duck1.jpg", "pics\\duck2.png", "pics\\duck3.jpg"];
+var duckImages = ["pics\\duck1.png", "pics\\duck2.png", "pics\\duck3.png"];
 
 var storageNums = [
     { cost: 1, amount: 20 },
@@ -81,6 +91,7 @@ if (savedGame !== null) {
   if (typeof gameData.cost === "undefined") gameData.cost = defaultValues.cost;
   if (typeof gameData.cost_increase_cost === "undefined") gameData.cost_increase_cost = defaultValues.cost_increase_cost;
   if (typeof gameData.cash === "undefined") gameData.cash = defaultValues.cash;
+  if (typeof gameData.lifetimeCash === "undefined") gameData.lifetimeCash = defaultValues.lifetimeCash;
   if (typeof gameData.eggcollectorNum === "undefined") gameData.eggcollectorNum = defaultValues.eggcollectorNum;
   if (typeof gameData.eggcollectorCost === "undefined") gameData.eggcollectorCost = defaultValues.eggcollectorCost;
   if (typeof gameData.click_increment === "undefined") gameData.click_increment = defaultValues.click_increment;
@@ -95,32 +106,33 @@ if (savedGame !== null) {
 
 } else {
     gameData = defaultValues;
-    console.log("NO SAVE FOUND, default values.")
+    console.log("NO SAVE FOUND, default values.");
 }
 
 
 // Hides the chooseDuck element and shows the duckNaming elements and chosen duck
 function duckChoose (num) {
-    document.getElementById("chooseDuck").style.display = "none";
-    document.getElementById("mainDuck").style.display = "block";
-    document.getElementById("nameDuck").style.display = "block";
-    document.getElementById("mainDuckImg").src=duckImages[num];
+    element["chooseDuck"].style.display = "none";
+    element["mainDuck"].style.display = "block";
+    element["nameDuck"].style.display = "block";
+    element["mainDuckImg"].src=duckImages[num];
     gameData.duckchosen = num;
 }
 
 //Gets ducks NAME from the input, changes the ducks NAME to NAME, hides the 
 function duckNamed (dName2) {
     if (dName2 == null) {
-        gameData.duckName = document.getElementById("dName").value;
+        gameData.duckName = element["dName"].value;
     } else {
         gameData.duckName = dName2;
     }
-    document.getElementById("dName2").innerHTML = gameData.duckName;
-    document.getElementById("dName2").style.display = "block";
-    document.getElementById("nameDuck").style.display = "none";
-    document.getElementById("mainDuckImg").setAttribute("onClick", "duckClicked()");
-    document.getElementById("buttons").style.display = "block";
-    document.getElementById("upgrades").style.display = "block";
+    element["dName2"].innerHTML = gameData.duckName;
+    element["dName2"].style.display = "block";
+    element["nameDuck"].style.display = "none";
+    element["mainDuckImg"].setAttribute("onClick", "duckClicked()");
+    element["buttons"].style.display = "block";
+    element["upgrades"].style.display = "block";
+    element["stats"].style.display = "block";
     duckUpdate();
 }
 
@@ -128,32 +140,34 @@ function duckNamed (dName2) {
 function duckClicked () {
     if (gameData.eggs + gameData.click_increment <= gameData.storage) {
         gameData.eggs += gameData.click_increment;
-        document.getElementById("totalEggsProg").value = 100 * (gameData.eggs/gameData.storage);
+        element["totalEggsProg"].value = 100 * (gameData.eggs/gameData.storage);
     } else if (gameData.eggs + gameData.click_increment > gameData.storage) {
         gameData.eggs = gameData.storage;
-        document.getElementById("totalEggsProg").value = 100 * (gameData.eggs/gameData.storage);
+        element["totalEggsProg"].value = 100 * (gameData.eggs/gameData.storage);
     }
     duckUpdate();
 }
 
 //Updates all BUTTONS and TEXT in the HTML, this is run every click and everytime a upgrade/autoclick event happens.
 function duckUpdate () {
-    document.getElementById("totalEggs").innerHTML = "Total eggs: " + gameData.eggs + "/" + gameData.storage;
-    document.getElementById("totalCash").innerHTML = "Cash: £" + gameData.cash.toFixed(2);
-    document.getElementById("totalEggsProg").value = 100 * (gameData.eggs/gameData.storage);
-    document.getElementById("eggPrice").innerHTML = "Price per egg: £" + gameData.cost.toFixed(2);
+    element["totalEggs"].innerHTML = "Total eggs: " + gameData.eggs + "/" + gameData.storage;
+    element["totalCash"].innerHTML = "Cash: £" + gameData.cash.toFixed(2);
+    element["totalEggsProg"].value = 100 * (gameData.eggs/gameData.storage);
+    element["eggPrice"].innerHTML = "Price per egg: £" + gameData.cost.toFixed(2);
     
     
-    document.getElementById("sellEggs").innerHTML = "Sell Eggs (£" + (gameData.eggs * gameData.cost).toFixed(2) + ")";
-    document.getElementById("sellEggsl").innerHTML = "Total eggs sold: " + gameData.eggsSold; 
+    element["sellEggs"].innerHTML = "Sell Eggs (£" + (gameData.eggs * gameData.cost).toFixed(2) + ")";
+    element["sellEggsl"].innerHTML = "Total eggs sold: " + gameData.eggsSold; 
     
-    document.getElementById("eggCollector").innerHTML = "Egg Collector (£" + gameData.eggcollectorCost.toFixed(2)+ ") (" + gameData.eggcollectorNum + ")";
-    document.getElementById("eggCollectorl").innerHTML = "Eggs collected per second: " + gameData.eggcollectorNum; 
+    element["eggCollector"].innerHTML = "Egg Collector (£" + gameData.eggcollectorCost.toFixed(2)+ ") (" + gameData.eggcollectorNum + ")";
+    element["eggCollectorl"].innerHTML = "Eggs collected per second: " + gameData.eggcollectorNum; 
     
-    document.getElementById("eggClickers").innerHTML = "Clicks per Egg (£" + gameData.click_increment_cost.toFixed(2) + ") (" + gameData.click_increment + ")";
-    document.getElementById("eggClickersl").innerHTML = "Eggs per click: "+ gameData.click_increment; 
+    element["eggClickers"].innerHTML = "Eggs per click (£" + gameData.click_increment_cost.toFixed(2) + ") (" + gameData.click_increment + ")";
+    element["eggClickersl"].innerHTML = "Eggs per click: "+ gameData.click_increment; 
     
-    document.getElementById("upgradeEgg").innerHTML = "Improve Egg Value (£" + gameData.cost_increase_cost.toFixed(2) + ")";
+    element["upgradeEgg"].innerHTML = "Improve Egg Value (£" + gameData.cost_increase_cost.toFixed(2) + ")";
+
+    element["lifetimeCash"].innerHTML = "Lifetime cash: £" + gameData.lifetimeCash.toFixed(2); 
     
     duckstoragebUpdate();
     buttonUpdate();
@@ -162,49 +176,32 @@ function duckUpdate () {
 //Updates colour of buttons if you can purchase the next upgrade
 function buttonUpdate () {
     //Storage button updater
-    if (gameData.cash >= storageNums[gameData.currentStorage].cost) {
-        document.getElementById("upgradeStorage").style.backgroundColor = "#4CAF50";
-    } else if (gameData.cash < storageNums[gameData.currentStorage].cost && gameData.currentStorage != storageNums.length) {
-        document.getElementById("upgradeStorage").style.backgroundColor = "#af4c4c";
-    } else { 
-        document.getElementById("upgradeStorage").style.backgroundColor = "#4CAF50";
+    if (gameData.currentStorage != storageNums.length) {
+        setBackgroundIf("upgradeStorage", gameData.cash >= storageNums[gameData.currentStorage].cost, "#4CAF50", "#af4c4c");
     }
-
-    //eggCollector Updater
-    let y = gameData.eggcollectorCost;
-    if (gameData.cash >= y) {
-        document.getElementById("eggCollector").style.backgroundColor = "#4CAF50";
-    } else if (gameData.cash < y) {
-        document.getElementById("eggCollector").style.backgroundColor = "#af4c4c";
-    }
-
-    //eggClickers Updater
-    if (gameData.cash >= gameData.click_increment_cost) {
-        document.getElementById("eggClickers").style.backgroundColor = "#4CAF50";
-    } else if (gameData.cash < gameData.click_increment_cost) {
-        document.getElementById("eggClickers").style.backgroundColor = "#af4c4c";
-    }
-
-    //eggCost Updator
-    if (gameData.cash >= gameData.cost_increase_cost) {
-        document.getElementById("upgradeEgg").style.backgroundColor = "#4CAF50";
-    } else if (gameData.cash < gameData.cost_increase_cost) {
-        document.getElementById("upgradeEgg").style.backgroundColor = "#af4c4c";
-    }
+    setBackgroundIf("eggCollector", gameData.cash >= gameData.eggcollectorCost, "#4CAF50", "#af4c4c");
+    setBackgroundIf("eggClickers", gameData.cash >= gameData.click_increment_cost, "#4CAF50", "#af4c4c");
+    setBackgroundIf("upgradeEgg", gameData.cash >= gameData.cost_increase_cost, "#4CAF50", "#af4c4c");
 }
+
+//Function that takes element, condition and colours and sets the element to that colour
+function setBackgroundIf(elementName, condition, trueColour, falseColour){
+    element[elementName].style.backgroundColor = condition ? trueColour : falseColour; 
+  }
 
 //Separate function that checks to see if storage upgrade has reached its max - separate to not clutter the duckUpdate function
 function duckstoragebUpdate () {
     if (gameData.currentStorage != storageNums.length) {
-        document.getElementById("upgradeStorage").innerHTML = "Upgrade Storage to [" + storageNums[gameData.currentStorage].amount + "] " + "(£" + storageNums[gameData.currentStorage].cost + ")"; 
+        element["upgradeStorage"].innerHTML = "Upgrade Storage to [" + storageNums[gameData.currentStorage].amount + "] " + "(£" + storageNums[gameData.currentStorage].cost + ")"; 
     } else {
-        document.getElementById("upgradeStorage").innerHTML = "Storage MAX";
+        element["upgradeStorage"].innerHTML = "Storage MAX";
     }
 }
 
 //When sell eggs is clicked sells total eggs in storage for their egg cost
 function sellEggs() {
     gameData.cash = gameData.cash + gameData.eggs * gameData.cost;
+    gameData.lifetimeCash += gameData.eggs * gameData.cost;
     gameData.eggsSold += gameData.eggs;
     gameData.eggs = 0;
     duckUpdate();
@@ -218,7 +215,7 @@ function upgradeStorage() {
         gameData.storage = storageNums[gameData.currentStorage].amount;
         gameData.cash -= storageNums[gameData.currentStorage].cost;
         gameData.currentStorage++;
-        duckUpdate()
+        duckUpdate();
     }
 }
     
@@ -226,23 +223,16 @@ function upgradeStorage() {
 //Main game loop that happens every 1 second
 var mainGameLoop = window.setInterval(function() {
     if (gameData.eggcollectorNum > 0) {
-    collectEggs()
+    collectEggs();
     } 
-    }, 1000)
+    }, 1000);
 
 //Function that turns egg collector on and handles buying new egg collectors, price increases by 10 x 1.1^(no. owned)
 function eggCollectorOn () {
-    let x = gameData.eggcollectorNum;
-    let y = gameData.eggcollectorCost;
-    if (x == 0 && gameData.cash >= y) {
-        gameData.eggcollectorNum = 1;
-        gameData.cash -= 10;
-        gameData.eggcollectorCost = 10 * Math.pow(1.1, x+1); 
-        duckUpdate();
-    } else if (x > 0 && gameData.cash >= y) {
+    if (gameData.eggcollectorNum >= 0 && gameData.cash >= gameData.eggcollectorCost) {
         gameData.eggcollectorNum++;
-        gameData.cash -= y;
-        gameData.eggcollectorCost = 10 * Math.pow(1.1, x+1); 
+        gameData.cash -= gameData.eggcollectorCost;
+        gameData.eggcollectorCost = 10 * Math.pow(1.1, gameData.eggcollectorNum + 1); 
         duckUpdate();
     }
     console.log(gameData.eggcollectorNum);
@@ -250,25 +240,20 @@ function eggCollectorOn () {
 
 //Increases amount of eggs on click
 function eggClickers() {
-    let x = gameData.click_increment_cost;
-    let y = gameData.click_increment;
-    if (x <= gameData.cash && y < 100) {
+    if (gameData.click_increment_cost <= gameData.cash && gameData.click_increment < 100) {
         gameData.click_increment++;
-        gameData.cash -= x;
-        gameData.click_increment_cost = 1 * Math.pow(1.4, y);
+        gameData.cash -= gameData.click_increment_cost;
+        gameData.click_increment_cost = 1 * Math.pow(1.4, gameData.click_increment);
         duckUpdate();
     }
 }
 
 //Upgrades the value of the egg
 function upgradeEgg() {
-    let x = gameData.cost_increase_cost;
-    let y = gameData.cost;
-    let z = gameData.cost_increase_num;
-    if (x <= gameData.cash) {
+    if (gameData.cost_increase_cost <= gameData.cash) {
         gameData.cost += 0.05;
-        gameData.cash -= x;
-        gameData.cost_increase_cost = 100 * Math.pow(1.4, z);
+        gameData.cash -= gameData.cost_increase_cost;
+        gameData.cost_increase_cost = 100 * Math.pow(1.4, gameData.cost_increase_num);
         gameData.cost_increase_num++;
         duckUpdate();
     }
@@ -277,11 +262,10 @@ function upgradeEgg() {
 
 //Collects eggs based on how many eggCollectors you have
 function collectEggs () {
-    let x = gameData.eggcollectorNum;
-    if (gameData.eggs + x <= gameData.storage) {
-        gameData.eggs += x;
+    if (gameData.eggs + gameData.eggcollectorNum <= gameData.storage) {
+        gameData.eggs += gameData.eggcollectorNum;
         duckUpdate();
-    } else if (gameData.eggs + x > gameData.storage) {
+    } else if (gameData.eggs + gameData.eggcollectorNum > gameData.storage) {
         gameData.eggs = gameData.storage;
         duckUpdate();
     }
@@ -300,24 +284,24 @@ function devSaveGame() {
 
 function devReset() {
     gameData = defaultValues;
-    localStorage.clear('duckgameSave')
+    localStorage.clear('duckgameSave');
     duckUpdate();
 }
 
 function devautoSave() {
     if (gameData.autosave == 1) {
         gameData.autosave = 0;
-        document.getElementById("devautosaveGame").innerHTML = "Autosave (OFF)";
+        element["devautosaveGame"].innerHTML = "Autosave (OFF)";
     } else if (gameData.autosave == 0) {
         gameData.autosave = 1;
-        document.getElementById("devautosaveGame").innerHTML = "Autosave (ON)";
+        element["devautosaveGame"].innerHTML = "Autosave (ON)";
     }
 }
 
 var saveGameLoop = window.setInterval(function() {
     if (gameData.autosave == 1) {
     localStorage.setItem('duckgameSave', JSON.stringify(gameData));
-    console.log("Autosaved")    
+    console.log("Autosaved");  
     }
-  }, 15000)
+  }, 15000);
 
