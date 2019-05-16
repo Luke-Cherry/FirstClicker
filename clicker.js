680fda5360da7bfbe8a848a8f02ed2101b8ec310
MrVisneski <38617758+MrVisneski@users.noreply.github.com>
2019-05-16 19:07:19 +0100

    Added egg counter at top of page

--- clicker.js
+++ clicker.js
@@ -172,6 +172,7 @@ function duckUpdate () {
     
     element["sellEggs"].innerHTML = "Sell Eggs (£" + (gameData.eggs * gameData.cost).toFixed(2) + ")";
     element["sellEggsl"].innerHTML = "Total eggs sold: " + gameData.eggsSold; 
+    element["eggCounter"].innerHTML = gameData.eggsSold; 
     
     element["eggCollector"].innerHTML = "Egg Collector (£" + gameData.eggcollectorCost.toFixed(2)+ ") (" + gameData.eggcollectorNum + ")";
     element["eggCollectorl"].innerHTML = "Eggs collected per second: " + gameData.eggcollectorNum; 
